df97355e5e7aa11f6b2f48e29306c7ca7e89daf7
Beaula12 <98835063+Beaula12@users.noreply.github.com>
2022-04-16 20:00:43 +0530

    Update deletepost.php

--- deletepost.php
+++ deletepost.php
@@ -2,7 +2,7 @@
 
 $con = new mysqli("localhost", "root", "", "tutorme");
 
-if($con->connect_error)
+if($conn->connect_error)
 {
     die ("connetion failed",$conn->connect_error);
 }
@@ -17,7 +17,7 @@ if (!isset($_SESSION['userlogin'])) {
 }
 else {
 	$user = $_SESSION['userlogin'];
-	$result = $con->query("SELECT * FROM user WHERE id='$user'");
+	$result = $conn->query("SELECT * FROM user WHERE id='$user'");
 		$get_user_email = mysqli_fetch_assoc($result);
 
 			$uname_db = $get_user_email['fullname'];
@@ -26,13 +26,13 @@ else {
 
 if (isset($_REQUEST['pid'])) {
 	$pstid = mysqli_real_escape_string($con, $_REQUEST['pid']);
-	$result3 = $con->query("SELECT * FROM post WHERE id='$pstid'");
+	$result3 = $conn->query("SELECT * FROM post WHERE id='$pstid'");
 		$get_user_pid = mysqli_fetch_assoc($result3);
 		$uid_db = $get_user_pid['postby_id'];
 	if($user != $uid_db){
 		header('location: post.php');
 	}else{
-		$result = $con->query("DELETE FROM post WHERE id='$pstid'");
+		$result = $conn->query("DELETE FROM post WHERE id='$pstid'");
 		header('location: profile.php?uid='.$user.'');
 	}
 }else {
