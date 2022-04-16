8e0c38a382a072589cc7f6eecd01a9096bfaf359
Beaula12 <98835063+Beaula12@users.noreply.github.com>
2022-04-16 13:59:11 +0530

    Add files via upload

---
+++ inc/connect.inc.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, "","tutorme");
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
