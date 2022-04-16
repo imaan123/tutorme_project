821ba4ce0ff416f1a94e162d0fe926ca249da8f7
Beaula12 <98835063+Beaula12@users.noreply.github.com>
2022-04-16 20:13:38 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['userlogin'])) {
 }
 else {
 	$user = $_SESSION['userlogin'];
-	$result = $con->query("SELECT * FROM user WHERE id='$user'");
+	$result = $conn->query("SELECT * FROM user WHERE id='$user'");
 		$get_user_name = $result->fetch_assoc();
 			$uname_db = $get_user_name['fullname'];
 			$email_db = $get_user_name['email'];
@@ -46,7 +46,7 @@ $repear_password = trim($repear_password);
 //update pass
 if ($senddata) {
 	//if the information submited
-	$password_query = $con->query("SELECT * FROM user WHERE id='$user'");
+	$password_query = $conn->query("SELECT * FROM user WHERE id='$user'");
 	while ($row = mysqli_fetch_assoc($password_query)) {
 		$db_password = $row['pass'];
 		$db_email = $row['email'];
@@ -60,7 +60,7 @@ if ($senddata) {
 					$error = "<p class='error_echo'>Sorry! But your new password must be 3 or more then 5 character!</p>";
 				}else {
 				$confirmCode   = substr( rand() * 900000 + 100000, 0, 6 );
-				$password_update_query = $con->query("UPDATE user SET pass='$newpassword_md5', confirmcode='$confirmCode', email='$email' WHERE id='$user'");
+				$password_update_query = $conn->query("UPDATE user SET pass='$newpassword_md5', confirmcode='$confirmCode', email='$email' WHERE id='$user'");
 				$error = "<p class='succes_echo'>Success! Your settings updated.</p>";
 			}
 		}else {
