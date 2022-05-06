b10463ce74432cf9dabb0bdefad0e258fa444bd8
Beaula12 <98835063+Beaula12@users.noreply.github.com>
2022-05-06 15:53:31 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -117,3 +117,210 @@ $_POST['first_name'] = trim($_POST['first_name']);
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Tutorme</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+	  <!-- Vendor CSS-->
+	  <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+     <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="container-fluid">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div style="padding-top:2px">
+                              <a href="index.html"><img src="images/logo1.png" alt="#" style="height:50px;width:150px"/></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarsExample04">
+                           <ul class="navbar-nav mr-auto">
+                              <li class="nav-item active">
+                                 <a class="nav-link" href="schedule.php"> Home </a>
+                              </li>
+                              <li class="nav-item">
+                              <?php
+                              if($usertype = "publicuser")
+                                 echo'<a class="nav-link" href="login.php">Posts</a>';
+                              else 
+                                 echo'<a class="nav-link" href="viewpost.php">Posts</a>';
+                              ?>
+                              </li>
+                              <li class="nav-item">
+                              <?php
+                              if($usertype = "publicuser")
+                                 echo'<a class="nav-link" href="login.php">Search Tutor</a>';
+                              else
+                                 echo'<a class="nav-link" href="search.php">Search Tutor</a>';
+                              ?>
+                              </li>
+                              <li class="nav-item">
+                                 <a class="nav-link" href="#contact">Contact</a>
+                              </li>
+                              <li class="nav-item d_none">
+                                 <a class="nav-link" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+                              </li>
+                              <li class=" d_none get_btn">
+                                 <a  href="login.php">Login</a>
+                              </li>
+                           </ul>
+                        </div>
+                     </nav>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </header>
+
+
+	<div  class="page-wrapper p-t-130 p-b-100 font-poppins" style="background:url('images/back1.jpg');">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-4">
+                <div class="card-body">
+				<?php
+				echo '<div class="signup_error_msg">';
+					
+						if (isset($error_message)) {echo $error_message;}
+						
+					
+				echo'</div>';
+				if(isset($success_message)) {echo $success_message;}
+				else echo'
+                    <h2 class="title" style="text-align:center;"><b>Registration Form</b></h2>
+                    <form method="POST">
+                        <hr>
+                        <br>
+                        <div class="row row-space">
+                            <div >
+							<label class="label"><b>Account Type</b></label>
+                                <div class="input-group">
+                                    
+                
+                                    <div class="p-t-10">
+                                        <label class="radio-container m-r-45" >teacher
+                                            <input type="radio"   name="account">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                        <label class="radio-container" >student/parent
+                                            <input type="radio" name="account">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </div>
+                                    
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <hr>
+                        <br>
+                        <div class="row row-space">
+                            <div >
+                                <div class="input-group">
+                                    <label class="label">full name</label>
+                                    <input class="input--style-4" type="text" name="first_name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+
+                            <div >
+							<label class="label"><b>Gender</b></label>
+                                <div class="input-group">
+                                    
+                                    <div class="p-t-10">
+                                        <label class="radio-container m-r-45">Male
+                                            <input type="radio"  name="gender">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                        <label class="radio-container">Female
+                                            <input type="radio" name="gender">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div >
+                                <div class="input-group">
+                                    <label class="label">Email</label>
+                                    <input class="input--style-4" type="email" name="email">
+                                </div>
+                            </div>
+                            <div >
+                                <div class="input-group">
+                                    <label class="label">Phone Number</label>
+                                    <input class="input--style-4" type="text" name="phone">
+                                </div>
+                            </div>
+                            <div >
+                                <div class="input-group">
+                                    <label class="label">Password</label>
+                                    <input class="input--style-4" type="password" name="password">
+                                </div>
+                            </div>
+                            <div >
+                                <div class="input-group">
+                                    <label class="label">Confirm Password</label>
+                                    <input class="input--style-4" type="password" name="cpassword">
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="p-t-15">
+                            <button class="btn btn--radius-2 btn--blue" type="submit">Submit</button>
+                        </div>
+                    </form>'
+					?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
